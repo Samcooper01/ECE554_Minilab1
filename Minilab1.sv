ce8482b0cefa1092caccab379ca9da8df0e7312a
Samcooper01 <coopsammy@gmail.com>
2025-01-28 17:56:21 -0600

    Add internal signals for Matrix A and B

--- Minilab1.sv
+++ Minilab1.sv
@@ -7,6 +7,20 @@ input clk;
 input rst_n;
 
 localparam MATRIX_COLUMNS_A = 8;
+localparam DATA_WIDTH = 8;
+
+//Matrix A Internal signals
+logic [DATA_WIDTH-1:0] datain_A [0:MATRIX_COLUMNS_A];
+logic [DATA_WIDTH-1:0] dataout_A [0:MATRIX_COLUMNS_A];
+
+logic rdreq_A, wrreq_A, rdempty_A, wrfull_A [0:MATRIX_COLUMNS_A];
+
+//Matrix B Internal signals
+logic [DATA_WIDTH-1:0] datain_B;
+logic [DATA_WIDTH-1:0] dataout_B;
+
+logic rdreq_B, wwreq_B, rdempty_B, wrfull_B;
+
 
 //8 MACS
 
@@ -16,18 +30,18 @@ localparam MATRIX_COLUMNS_A = 8;
 generate
 
   //Matrix A FIFOS
-  for (i=0; i<MATRIX_COLUMNS_A; i=i+1) begin : fifo_gen
+  for (integer i=0; i<MATRIX_COLUMNS_A; i=i+1) begin : fifo_gen
     FIFO input_fifo
     (
     .aclr(rst_n),
-	  .data(),
+	  .data(datain_A[i]),
 	  .rdclk(clk),
-	  .rdreq(),
+	  .rdreq(rdreq_A[i]),
 	  .wrclk(clk),
-	  .wrreq(),
-	  .q(),
-	  .rdempty(),
-	  .wrfull()
+	  .wrreq(wrreq_A[i]),
+	  .q(dataout_A[i]),
+	  .rdempty(rdempty_A[i]),
+	  .wrfull(wrfull_A[i])
     );
   end
 
@@ -35,14 +49,14 @@ generate
       FIFO input_fifo
     (
     .aclr(rst_n),
-	  .data(),
+	  .data(datain_B),
 	  .rdclk(clk),
-	  .rdreq(),
+	  .rdreq(rdreq_B),
 	  .wrclk(clk),
-	  .wrreq(),
-	  .q(),
-	  .rdempty(),
-	  .wrfull()
+	  .wrreq(wwreq_B),
+	  .q(dataout_B),
+	  .rdempty(rdempty_B),
+	  .wrfull(wrfull_B)
     );
 
 
