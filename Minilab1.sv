9e35a3a65afc3d26888a58fb1a42573573efd6de
jneau <jneau@ad.engr.wisc.edu>
2025-01-28 17:48:18 -0600

    set proper inputs for mac

--- Minilab1.sv
+++ Minilab1.sv
@@ -1,11 +1,32 @@
 module Minilab1 (
     input wire clk,
-    input wire reset_n
+    input wire rst_n
 );
 
 //8 MACS
-
-
+logic [8:0] En;
+logic [7:0] Ain;
+logic [8:0] Bin;
+logic [7:0] Couts;
+generate
+  for (i=0; i<8; i=i+1) begin : fifo_gen
+    MAC 
+    #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) element_mac
+    (
+        .clk(clk),
+        .rst_n(rst_n),
+        .En(En[7:0]),
+        .Clr(Clr),
+        .Ain(Ain[7:0]),
+        .Bin(Bin[7:0]),
+        .Couts(Couts[7:0]),
+        .EnOut(EnOut[8:1]),
+        .Bout(Bin[8:1]),
+    );
+  end
+endgenerate
 
 //9 FIFOS
 generate
