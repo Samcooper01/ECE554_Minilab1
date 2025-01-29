23f0ea6a233d58cd63ee38afd9caba73a85aec0d
jneau <jneau@ad.engr.wisc.edu>
2025-01-28 18:02:25 -0600

    re-add remove MAC

--- Minilab1.sv
+++ Minilab1.sv
@@ -23,7 +23,29 @@ logic rdreq_B, wwreq_B, rdempty_B, wrfull_B;
 
 
 //8 MACS
-
+logic [8:0] En;
+logic [7:0] Ain;
+logic [8:0] Bin;
+logic [7:0] Couts;
+generate
+  for (i=0; i<8; i=i+1) begin : fifo_gen
+    MAC 
+    #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) element_mac
+    (
+        .clk(clk),
+        .rst_n(rst_n),
+        .En(En[7:0]),
+        .Clr(Clr),
+        .Ain(Ain[7:0]),
+        .Bin(Bin[7:0]),
+        .Couts(Couts[7:0]),
+        .EnOut(EnOut[8:1]),
+        .Bout(Bin[8:1]),
+    );
+  end
+endgenerate
 
 
 //9 FIFOS
