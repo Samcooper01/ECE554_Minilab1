1809b71eb698b7844491792a85b4363ab5e59c73
jneau <jneau@ad.engr.wisc.edu>
2025-02-04 14:23:13 -0600

    Fix Sam's horrible code

--- Minilab1.sv
+++ Minilab1.sv
@@ -1,10 +1,32 @@
 module Minilab1 (
-    clk,
-    rst_n
+    
+
+	//////////// CLOCK //////////
+	input 		          		CLOCK2_50,
+	input 		          		CLOCK3_50,
+	input 		          		CLOCK4_50,
+	input 		          		CLOCK_50,
+
+	//////////// SEG7 //////////
+	output	reg	     [6:0]		HEX0,
+	output	reg	     [6:0]		HEX1,
+	output	reg	     [6:0]		HEX2,
+	output	reg	     [6:0]		HEX3,
+	output	reg	     [6:0]		HEX4,
+	output	reg	     [6:0]		HEX5,
+	
+	//////////// LED //////////
+	output		     [9:0]		LEDR,
+
+	//////////// KEY //////////
+	input 		     [3:0]		KEY,
+
+	//////////// SW //////////
+	input 		     [9:0]		SW
 );
 
-input clk;
-input rst_n;
+assign clk = CLOCK_50;
+assign rst_n = KEY[0];
 
 localparam MATRIX_COLUMNS_A = 8;
 localparam DATA_WIDTH = 8;
@@ -65,6 +87,7 @@ logic [23:0] Couts [7:0];
 logic read_B;
 logic read_A [7:0];
 logic rdempty_0_ff;
+logic rdempty_0_ff2;
 logic rdempty_1_ff;
 logic rdempty_2_ff;
 logic rdempty_3_ff;
@@ -117,7 +140,7 @@ generate
       .aclr(~rst_n),
       .data(datain),
       .rdclk(clk),
-      .rdreq(read_A[z]),
+      .rdreq(En[z] | preread),
       .wrclk(clk),
       .wrreq(wrreq_A[z]),
       .q(a_out[z]),
@@ -278,8 +301,11 @@ assign read_A[7] = (~rst_n) ? 0 : (start_read);
 always_ff @(posedge clk) begin
   rdempty_0_ff <= rdempty_A[0];
 end
+always_ff @(posedge clk) begin
+  rdempty_0_ff2 <= rdempty_0_ff;
+end
 
-assign En[0] = start_calc & ~rdempty_0_ff;
+assign En[0] = start_calc & ~rdempty_0_ff2;
 
 // next state flop
 always @(posedge clk, negedge rst_n) begin
@@ -317,6 +343,7 @@ always_comb begin
       FILL_FIFO: begin
         if(fifo_all_full) begin
           next_state = WAIT;
+          preread = 1;
         end
         else begin
           next_state = FILL_FIFO;
@@ -324,6 +351,7 @@ always_comb begin
         end
       end
       WAIT: begin
+        preread = 0;
         next_state = CALC;
         start_read = 1;
       end
@@ -347,5 +375,206 @@ always_comb begin
     endcase
 end
 
+logic [23:0] macout;
+logic SW_pushed;
+
+always_comb begin
+  SW_pushed = SW[0] | SW[1] | SW[2] | SW[3] | SW[4] | SW[5] | SW[6] | SW[7];
+  if (SW[0])
+    macout = Couts[0];
+  else if (SW[1])
+    macout = Couts[1];
+  else if (SW[2])
+    macout = Couts[2];
+  else if (SW[3])
+    macout = Couts[3];
+  else if (SW[4])
+    macout = Couts[4];
+  else if (SW[5])
+    macout = Couts[5];
+  else if (SW[6])
+    macout = Couts[6];
+  else if (SW[7])
+    macout = Couts[7];
+  else
+    macout = 0;
+end
+
+
+parameter HEX_0 = 7'b1000000;		// zero
+parameter HEX_1 = 7'b1111001;		// one
+parameter HEX_2 = 7'b0100100;		// two
+parameter HEX_3 = 7'b0110000;		// three
+parameter HEX_4 = 7'b0011001;		// four
+parameter HEX_5 = 7'b0010010;		// five
+parameter HEX_6 = 7'b0000010;		// six
+parameter HEX_7 = 7'b1111000;		// seven
+parameter HEX_8 = 7'b0000000;		// eight
+parameter HEX_9 = 7'b0011000;		// nine
+parameter HEX_10 = 7'b0001000;	// ten
+parameter HEX_11 = 7'b0000011;	// eleven
+parameter HEX_12 = 7'b1000110;	// twelve
+parameter HEX_13 = 7'b0100001;	// thirteen
+parameter HEX_14 = 7'b0000110;	// fourteen
+parameter HEX_15 = 7'b0001110;	// fifteen
+parameter OFF   = 7'b1111111;		// all off
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[3:0])
+      4'd0: HEX0 = HEX_0;
+	   4'd1: HEX0 = HEX_1;
+	   4'd2: HEX0 = HEX_2;
+	   4'd3: HEX0 = HEX_3;
+	   4'd4: HEX0 = HEX_4;
+	   4'd5: HEX0 = HEX_5;
+	   4'd6: HEX0 = HEX_6;
+	   4'd7: HEX0 = HEX_7;
+	   4'd8: HEX0 = HEX_8;
+	   4'd9: HEX0 = HEX_9;
+	   4'd10: HEX0 = HEX_10;
+	   4'd11: HEX0 = HEX_11;
+	   4'd12: HEX0 = HEX_12;
+	   4'd13: HEX0 = HEX_13;
+	   4'd14: HEX0 = HEX_14;
+	   4'd15: HEX0 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX0 = OFF;
+  end
+end
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[7:4])
+      4'd0: HEX1 = HEX_0;
+	   4'd1: HEX1 = HEX_1;
+	   4'd2: HEX1 = HEX_2;
+	   4'd3: HEX1 = HEX_3;
+	   4'd4: HEX1 = HEX_4;
+	   4'd5: HEX1 = HEX_5;
+	   4'd6: HEX1 = HEX_6;
+	   4'd7: HEX1 = HEX_7;
+	   4'd8: HEX1 = HEX_8;
+	   4'd9: HEX1 = HEX_9;
+	   4'd10: HEX1 = HEX_10;
+	   4'd11: HEX1 = HEX_11;
+	   4'd12: HEX1 = HEX_12;
+	   4'd13: HEX1 = HEX_13;
+	   4'd14: HEX1 = HEX_14;
+	   4'd15: HEX1 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX1 = OFF;
+  end
+end
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[11:8])
+      4'd0: HEX2 = HEX_0;
+	   4'd1: HEX2 = HEX_1;
+	   4'd2: HEX2 = HEX_2;
+	   4'd3: HEX2 = HEX_3;
+	   4'd4: HEX2 = HEX_4;
+	   4'd5: HEX2 = HEX_5;
+	   4'd6: HEX2 = HEX_6;
+	   4'd7: HEX2 = HEX_7;
+	   4'd8: HEX2 = HEX_8;
+	   4'd9: HEX2 = HEX_9;
+	   4'd10: HEX2 = HEX_10;
+	   4'd11: HEX2 = HEX_11;
+	   4'd12: HEX2 = HEX_12;
+	   4'd13: HEX2 = HEX_13;
+	   4'd14: HEX2 = HEX_14;
+	   4'd15: HEX2 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX2 = OFF;
+  end
+end
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[15:12])
+      4'd0: HEX3 = HEX_0;
+	   4'd1: HEX3 = HEX_1;
+	   4'd2: HEX3 = HEX_2;
+	   4'd3: HEX3 = HEX_3;
+	   4'd4: HEX3 = HEX_4;
+	   4'd5: HEX3 = HEX_5;
+	   4'd6: HEX3 = HEX_6;
+	   4'd7: HEX3 = HEX_7;
+	   4'd8: HEX3 = HEX_8;
+	   4'd9: HEX3 = HEX_9;
+	   4'd10: HEX3 = HEX_10;
+	   4'd11: HEX3 = HEX_11;
+	   4'd12: HEX3 = HEX_12;
+	   4'd13: HEX3 = HEX_13;
+	   4'd14: HEX3 = HEX_14;
+	   4'd15: HEX3 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX3 = OFF;
+  end
+end
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[19:16])
+      4'd0: HEX4 = HEX_0;
+	   4'd1: HEX4 = HEX_1;
+	   4'd2: HEX4 = HEX_2;
+	   4'd3: HEX4 = HEX_3;
+	   4'd4: HEX4 = HEX_4;
+	   4'd5: HEX4 = HEX_5;
+	   4'd6: HEX4 = HEX_6;
+	   4'd7: HEX4 = HEX_7;
+	   4'd8: HEX4 = HEX_8;
+	   4'd9: HEX4 = HEX_9;
+	   4'd10: HEX4 = HEX_10;
+	   4'd11: HEX4 = HEX_11;
+	   4'd12: HEX4 = HEX_12;
+	   4'd13: HEX4 = HEX_13;
+	   4'd14: HEX4 = HEX_14;
+	   4'd15: HEX4 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX4 = OFF;
+  end
+end
+
+always @(*) begin
+  if (state == DONE & SW_pushed) begin
+    case(macout[23:20])
+      4'd0: HEX5 = HEX_0;
+	   4'd1: HEX5 = HEX_1;
+	   4'd2: HEX5 = HEX_2;
+	   4'd3: HEX5 = HEX_3;
+	   4'd4: HEX5 = HEX_4;
+	   4'd5: HEX5 = HEX_5;
+	   4'd6: HEX5 = HEX_6;
+	   4'd7: HEX5 = HEX_7;
+	   4'd8: HEX5 = HEX_8;
+	   4'd9: HEX5 = HEX_9;
+	   4'd10: HEX5 = HEX_10;
+	   4'd11: HEX5 = HEX_11;
+	   4'd12: HEX5 = HEX_12;
+	   4'd13: HEX5 = HEX_13;
+	   4'd14: HEX5 = HEX_14;
+	   4'd15: HEX5 = HEX_15;
+    endcase
+  end
+  else begin
+    HEX5 = OFF;
+  end
+end
+
+assign LEDR = {{8{1'b0}}, state};
 
 endmodule
